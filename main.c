d0ba18f422ef0ac28c880cee1f4a4d3b73963e04
LucasLCO <lucaslco2005@gmail.com>
2022-10-23 17:56:09 -0300

    score files working

--- main.c
+++ main.c
@@ -9,8 +9,7 @@ typedef struct player{
 
 
 void write(char arq[], player *one);
-void read(char arq[], player *one);
-
+void read(char arq[], int bool);
 
 int main(void){
     player one;
@@ -18,7 +17,7 @@ int main(void){
     int op;
     do{
         system("cls");
-        printf("[1]Play\n[2]Scores\n: ");
+        printf("[1]Play\n[2]Scores\n[3]Leave\n: ");
         scanf("%i", &op);
         switch(op){
             case 1:
@@ -27,18 +26,44 @@ int main(void){
                 fflush(stdin);
                 one.score_value = play();
                 write(arq,&one);
+                system("cls");
+                read(arq,0);
+                system("pause");
                 break;
             case 2:
-                
+                read(arq,1);
+                system("pause");
                 break;
+            case 3:
+                return 0;
         }
-    }while(op != 1 && op != 2);
+    }while(op != 3);
 }
 
 void write(char arq[], player *one){
     FILE *score = fopen(arq,"ab");
     if (score){
         fwrite(one, sizeof(player),1, score);
+        fclose(score);
+    }
+    else
+        printf("There is @!#$");
+}
+
+void read(char arq[], int bool){
+    player two;
+    FILE *score = fopen(arq,"rb");
+    if(score){
+        while(!(feof(score))){
+            if(bool==1){
+                if(fread(&two,sizeof(player),1,score))
+                printf("\nPlayer: %s\nScore: %i\n",two.name,two.score_value);            
+            }
+            else{
+                if(!(fread(&two,sizeof(player),1,score)))
+                printf("\n%s Score: %i\n",two.name,two.score_value);    
+            }
+        }
         fclose(score);
     }
     else
